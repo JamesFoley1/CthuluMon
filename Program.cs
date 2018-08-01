f469e99b173d2a369f17859a139f492f5e9537fa
kyle_N <kylecnovak@gmail.com>
2018-08-01 12:24:20 -0700

    added monster generator and attack list

--- Program.cs
+++ Program.cs
@@ -6,57 +6,13 @@ namespace CthuluMon
     {
         static void Main(string[] args)
         {
-            Player Player = GetPlayer();
-            Monster Monster1 = GenerateMonster();
-            Monster Monster2 = GenerateMonster();
-            Monster1.getInfo();
-            Monster2.getHealth();
-            Player.MindControl(Monster1, Monster2);
-            System.Console.WriteLine("Mind Controlling!");
-            Monster1.getHealth();
-            Monster2.getHealth();
-            
-            // System.Console.WriteLine($"A Monster {Monster1} appears! What would you like to do?");
-            // StartEncounter(Player);
+
         }
 
-        // public static void StartEncounter(Player _player){
-        //     Player Player = _player;
-        //     string InputLine = Console.ReadLine();
-        //     switch (InputLine){
-        //         case $"{}":
-        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
-        //             break;
-        //         case $"{}":
-        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
-        //             break;
-        //         case $"{}":
-        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
-        //             break;
-        //         case $"{}":
-        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
-        //             break;
-        //         case $"{}":
-        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
-        //             break;
-        //         case "Run":
-        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
-        //             break;
 
-        //         default:
-        //             break;
-        //     }
-        //     System.Console.WriteLine(InputLine);
-        // }
         public static Player GetPlayer(){
             Player Player = new Player();
             return Player;
-        }
-
-        public static Monster GenerateMonster(){
-            Monster FSM = new Monster(1, "Flying Spaghetti Monster");
-            getStats(FSM);
-            return FSM;
         }
 
         public static void getStats(Monster monster){
