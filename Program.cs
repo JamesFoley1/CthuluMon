2ec43942d4d66c1188f768a8acae4ec7e5e301c4
James-MacBook-Pro <jrfoley13866@yahoo.com>
2018-07-31 17:59:59 -0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CthuluMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
