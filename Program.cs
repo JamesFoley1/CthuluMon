91022e77a903bdbf6d8202e5b7369b3f51bb776b
James-MacBook-Pro <jrfoley13866@yahoo.com>
2018-07-31 21:41:19 -0700

    Experimental Encounters

--- Program.cs
+++ Program.cs
@@ -6,12 +6,49 @@ namespace CthuluMon
     {
         static void Main(string[] args)
         {
-            GenerateMonster();
+            Player Player = GetPlayer();
+            Monster Monster1 = GenerateMonster();
+            System.Console.WriteLine($"A Monster {Monster1} appears! What would you like to do?");
+            // StartEncounter(Player);
         }
 
-        public static void GenerateMonster(){
+        // public static void StartEncounter(Player _player){
+        //     Player Player = _player;
+        //     string InputLine = Console.ReadLine();
+        //     switch (InputLine){
+        //         case $"{}":
+        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
+        //             break;
+        //         case $"{}":
+        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
+        //             break;
+        //         case $"{}":
+        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
+        //             break;
+        //         case $"{}":
+        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
+        //             break;
+        //         case $"{}":
+        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
+        //             break;
+        //         case "Run":
+        //             System.Console.WriteLine("You ran away?! I am deeply ashamed.");
+        //             break;
+
+        //         default:
+        //             break;
+        //     }
+        //     System.Console.WriteLine(InputLine);
+        // }
+        public static Player GetPlayer(){
+            Player Player = new Player();
+            return Player;
+        }
+
+        public static Monster GenerateMonster(){
             Monster FSM = new Monster(1, "Flying Spaghetti Monster");
             getStats(FSM);
+            return FSM;
         }
 
         public static void getStats(Monster monster){
